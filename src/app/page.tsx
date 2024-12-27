872c777dcc31f71f5eb1eae4a1e147bd7a17119f
abdul-busybrain <awwalhajine@gmail.com>
2024-12-27 04:46:42 +0100

    Added Social Login with Clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -6,6 +7,12 @@ export default function Home() {
     <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Billinfo</h1>
       <p>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <Button asChild>
           <Link href="/dashboard">Sign in</Link>
         </Button>
