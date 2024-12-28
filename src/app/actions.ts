3171142c8ca07d32cb6bc605929cb3ff616c03d3
abdul-busybrain <awwalhajine@gmail.com>
2024-12-28 14:36:03 +0100

    Secured Server Actions with Clerk

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,14 +1,20 @@
 "use server";
 
-import { db } from "@/db";
-import { Invoices } from "@/db/schema";
-
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
+
+import { Invoices } from "@/db/schema";
+import { db } from "@/db";
 
 export async function createAction(formData: FormData) {
+  const { userId } = auth();
   const value = Math.floor(parseFloat(String(formData.get("value"))) * 100);
   console.log(value);
   const description = formData.get("description") as string;
+
+  if (!userId) {
+    return;
+  }
 
   const results = await db
     .insert(Invoices)
